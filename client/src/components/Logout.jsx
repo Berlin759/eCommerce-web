d849b40387668d626c497377e724611cbf336375
ronak.dobariya <ronak.dobariya@bitrixinfotech.com>
2025-12-08 14:42:06 +0530

    TC

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Button } from "./ui/button";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { removeUser } from "../redux/orebiSlice";
 
 const Logout = () => {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const handleLogout = async () => {
         localStorage.removeItem("token");
+        dispatch(removeUser());
         toast.success("log out successfully");
-        navigate("/");
+        // navigate("/");
+        window.location.href = "/";
     };
     return (
         <Button onClick={handleLogout} className="px-8 py-2.5">
