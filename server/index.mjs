f2a4e353a68452d909859f94b09b391ed57f5663
Berlin Marco <belinmartin759@gmail.com>
2026-08-29 09:53:09 +0530

    TC

--- server/index.mjs
+++ server/index.mjs
@@ -52,11 +52,6 @@ app.use(
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
-app.use((req, res, next) => {
-    console.log("🔥 REQUEST:", req.method, req.originalUrl);
-    next();
-});
-
 // dbConnect();
 // connectCloudinary();
 
